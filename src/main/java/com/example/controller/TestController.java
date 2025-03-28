2d486cb409575c8bc8d490e2008c1058bd6c5af9
Govind Bhosle <govindbhosle371@gmail.com>
2025-03-28 14:34:09 +0530

    project is added

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("wish/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "Good Morning "+name;
+	}
+}
